820802d0492baabdf53e2ba416bea09a28cc2fb1
MTUSTA <mtusta113@gmail.com>
2022-07-20 00:28:38 +0300

    zipkin logging eklendi
    zipkin java 8 ile çalışmasından dolayı projeden ayrı olarak ayarlandı
    
    zipkin en son sürümleri docker compose da çalışacak şekilde ayarlanmış bundan dolayı 2.11.7 sürümü kullandım.
    
    şu an log storage durumu yok sonraki committe ayarlanacak

--- MicroService-App/Service-Parent/Hesap-Microservice/src/main/java/com/companyname/hesapmicroservice/repositories/HesapRepository.java
+++ MicroService-App/Service-Parent/Hesap-Microservice/src/main/java/com/companyname/hesapmicroservice/repositories/HesapRepository.java
@@ -3,6 +3,9 @@ package com.companyname.hesapmicroservice.repositories;
 import com.companyname.hesapmicroservice.entities.Hesap;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//@Repository
+// When using spring data jpa you don't need to specify repository explicitly.
+// The spring data jpa scans for all interfaces extending repository and generate implementation automatically.
 public interface HesapRepository extends JpaRepository<Hesap, Long> {
 
 }
